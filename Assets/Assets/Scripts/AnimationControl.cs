4f161c51a26b762d0f7be71a7fc55f5272dfaa71
Xindeling Pan <xindeling@gmail.com>
2019-01-19 16:05:51 -0500

    enable hand to open the letter

--- Assets/Assets/Scripts/AnimationControl.cs
+++ Assets/Assets/Scripts/AnimationControl.cs
@@ -6,13 +6,19 @@ using UnityEngine;
 public class AnimationControl : MonoBehaviour {
     public GameObject Object;
     public AnimationClip Clip;
+    private Animator anm;
 	// Use this for initialization
 	void Start () {
-		
+        anm = GetComponent<Animator>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
 }
