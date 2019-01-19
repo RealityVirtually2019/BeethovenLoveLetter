6b0b547eba676342df7833c89915c02f7ee9d356
Xindeling Pan <xindeling@gmail.com>
2019-01-19 14:59:04 -0500

    add trees

---
+++ Assets/Fabulous_trees/ground/scripts/WaterfallTextures.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterfallTextures : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+}
