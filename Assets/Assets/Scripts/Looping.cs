5066c6698eb71d508af44dc239f7bd2a40a76d96
Xindeling Pan <xindeling@gmail.com>
2019-01-20 12:25:00 -0500

    adjust animation

---
+++ Assets/Assets/Scripts/Looping.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looping : MonoBehaviour {
+    Animator Anm;
+    private float timer = 0f;
+    // Use this for initialization
+    void Start () {
+        Anm = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer > 5)
+        {
+            Anm.enabled = true;
+            timer = 0f;
+        }
+        else
+        {
+           // Anm.enabled = true;
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    Animator anm;
+    private float timer = 0f;
+    public float Timers;
+	// Use this for initialization
+	void Start () {
+        anm = GetComponent<Animator>();
+        anm.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(Timers < timer)
+        {
+            anm.enabled = true;
+        }
+	}
+}
