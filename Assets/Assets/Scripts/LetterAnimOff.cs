aa69efd69b91f994ae6be0cd579e2b6764f72a1c
Xindeling Pan <xindeling@gmail.com>
2019-01-19 20:22:59 -0500

    add collider for hands,letter and animate the letter

---
+++ Assets/Assets/Scripts/LetterAnimOff.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterAnimOff : MonoBehaviour {
+    Animator Anm;
+	void Start () {
+        Anm = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Anm.enabled = false;
+   
+    }
+}
