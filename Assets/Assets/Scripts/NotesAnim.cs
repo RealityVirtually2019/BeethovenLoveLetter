115e7beb068c2d660da8d93e04fcf3713748b5eb
Xindeling Pan <xindeling@gmail.com>
2019-01-20 01:13:20 -0500

    added collider to open the letter

---
+++ Assets/Assets/Scripts/NotesAnim.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesAnim : MonoBehaviour {
+    Animator Anm;
+    public AudioClip Clip;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        Anm = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
+        Anm.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Anm.enabled == false)
+        {
+            Anm.enabled = true;
+            source.Play();
+            Debug.Log("print");
+        }
+        else
+        {
+            source.Play();
+        }
+    }
+}
