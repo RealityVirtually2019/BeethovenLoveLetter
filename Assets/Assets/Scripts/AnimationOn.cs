949baca54da697036abfb4db8cd39fbd68350a71
Xindeling Pan <xindeling@gmail.com>
2019-01-19 22:26:28 -0500

    add more trees and audio

---
+++ Assets/Assets/Scripts/AnimationOn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationOn : MonoBehaviour {
+    Animator Anm;
+    // Use this for initialization
+    void Start () {
+        Anm = GetComponent<Animator>();
+        //Anm.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Anm.enabled = true;
+
+    }
+}
